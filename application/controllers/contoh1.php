29e65f4f87ea8ce4f8e9190d918ef1ce47ff658a
aknopan <adhyananovanda23@gmail.com>
2022-09-19 16:46:48 +0700

    tugas

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends   CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya novan 
+            saya tinggal di rasau jaya 
+            olah raga favorit saya banyak 
+            salah satunya jogging";
+    }
+}
