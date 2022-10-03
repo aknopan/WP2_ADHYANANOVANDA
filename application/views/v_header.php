e30da68662e2476be4526d1e8629a9e1e74aff30
aknopan <adhyananovanda23@gmail.com>
2022-10-03 16:50:17 +0700

    tugas pertemuan 4

--- application/views/v_header.php
+++ application/views/v_header.php
@@ -19,8 +19,9 @@ CodeIgniter</h3>
                 <ul>
                     <li><a href=”<?php echo 
 base_url().'index.php/web' ?>”>Home</a></li>
- <li><a href=”<?php echo 
-base_url().'index.php/web/about' ?>”>About</a></li>
+ <li><a href="<?php echo base_url().'index.php/web' ?>">Home</a></li>
+                    <li><a href="<?php echo 
+base_url().'index.php/web/about' ?>">About</a></li>
                 </ul>
             </nav>
             <div class=”clear”></div>
